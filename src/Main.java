96aea999b55ec6e35660d959952f49d0ca5fb75e
mukeshchilukoti <31955448+mukeshchilukoti@users.noreply.github.com>
2017-09-14 16:12:17 +0530

    print

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Enter your name");
 		
 		//You may test that your code works find here
 		//Please check that your code works and has no 
